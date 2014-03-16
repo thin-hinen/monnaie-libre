3a1f182bd7feaf43662eee340a0c1ca4d2f6f83f
Oualid <oualid.manai@hotmail.fr>
2014-03-16 13:30:48 +0100

    ajout bundle prestation + entité prestation

---
+++ Symfony/src/Ml/PrestationBundle/Entity/Prestation.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Ml\PrestationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Prestation
+ *
+ * @ORM\Table(name="prestation")
+ * @ORM\Entity(repositoryClass="Ml\PrestationBundle\Entity\PrestationRepository")
+ */
+abstract class Prestation
+{
+
+    /**
+    * @ORM\ManyToOne(targetEntity="Ml\UserBundle\Entity\User")
+    * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateCreation", type="datetime")
+     */
+    private $dateCreation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="commentaire", type="text")
+     */
+    private $commentaire;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="signaler", type="integer")
+     */
+    private $signaler;
+
+    
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="visibilite", type="boolean")
+     */
+    private $visibilite;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     * @return Prestation
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string 
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set dateCreation
+     *
+     * @param \DateTime $dateCreation
+     * @return Prestation
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation
+     *
+     * @return \DateTime 
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set commentaire
+     *
+     * @param string $commentaire
+     * @return Prestation
+     */
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * Get commentaire
+     *
+     * @return string 
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+    /**
+     * Set signaler
+     *
+     * @param integer $signaler
+     * @return Prestation
+     */
+    public function setSignaler($signaler)
+    {
+        $this->signaler = $signaler;
+
+        return $this;
+    }
+
+    /**
+     * Get signaler
+     *
+     * @return integer 
+     */
+    public function getSignaler()
+    {
+        return $this->signaler;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param integer $prix
+     * @return Prestation
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return integer 
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set visibilite
+     *
+     * @param boolean $visibilite
+     * @return Prestation
+     */
+    public function setVisibilite($visibilite)
+    {
+        $this->visibilite = $visibilite;
+
+        return $this;
+    }
+
+    /**
+     * Get visibilite
+     *
+     * @return boolean 
+     */
+    public function getVisibilite()
+    {
+        return $this->visibilite;
+    }
+}
